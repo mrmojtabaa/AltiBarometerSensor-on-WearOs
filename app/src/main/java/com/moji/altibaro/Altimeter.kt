eb703a00b0b848ef5250a1ee685ce4b2a294442e
Mojtaba Haddadi <mojtaba.haddadi@snapp.cab>
2023-07-20 14:33:49 +0330

    First init

---
+++ app/src/main/java/com/moji/altibaro/Altimeter.kt
@@ -0,0 +1,8 @@
+package com.moji.altibaro
+
+import androidx.wear.watchface.complications.datasource.SuspendingComplicationDataSourceService
+
+
+class Altimeter : SuspendingComplicationDataSourceService() {
+
+}
---
+++ app/src/main/java/com/moji/altibaro/AltimeterBroadcastReceiver.kt
@@ -0,0 +1,13 @@
+package com.moji.altibaro
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AltimeterBroadcastReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        //TODO("AltimeterBroadcastReceiver.onReceive() is not implemented")
+    }
+}
