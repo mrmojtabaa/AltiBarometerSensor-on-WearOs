d16f29ef6592fdcab09a153bb735ce85d3f4ec23
Mojtaba Haddadi <mojtaba.haddadi@snapp.cab>
2023-07-20 15:42:32 +0330

    Fix build issues

--- app/src/main/java/com/moji/altibaro/Altimeter.kt
+++ app/src/main/java/com/moji/altibaro/Altimeter.kt
@@ -1,8 +1,18 @@
 package com.moji.altibaro
 
-import androidx.wear.watchface.complications.datasource.SuspendingComplicationDataSourceService
+import androidx.wear.watchface.complications.data.ComplicationData
+import androidx.wear.watchface.complications.data.ComplicationType
+import androidx.wear.watchface.complications.datasource.ComplicationDataSourceService
+import androidx.wear.watchface.complications.datasource.ComplicationRequest
 
+class Altimeter : ComplicationDataSourceService() {
+    override fun getPreviewData(type: ComplicationType): ComplicationData? {
+        TODO("Not yet implemented")
+    }
 
-class Altimeter : SuspendingComplicationDataSourceService() {
+    override fun onComplicationRequest(request: ComplicationRequest, listener: ComplicationRequestListener) {
+        TODO("Not yet implemented")
+    }
+
 
 }
